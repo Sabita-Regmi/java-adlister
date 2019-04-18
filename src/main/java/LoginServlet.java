bc089e3b960d5a972aaed02315d8cc863b6a254a
Sabita-Regmi <sabitaregmi3291@gmail.com>
2019-04-18 15:12:11 -0500

    exercise

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,25 +1,36 @@
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Date;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
-    }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        boolean validAttempt = username.equals("admin") && password.equals("password");
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        if (req.getSession().getAttribute("user") != null) {
+            res.sendRedirect("/profile");
 
-        if (validAttempt) {
-            response.sendRedirect("/profile");
         } else {
-            response.sendRedirect("/login");
+            req.getRequestDispatcher("WEB-INF/login.jsp").forward(req, res);
+        }
+    }
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        if (req.getMethod().equalsIgnoreCase("post")) {
+            String username = req.getParameter("username");
+            String password = req.getParameter("password");
+
+            if (username.equals("admin") && password.equals("password")) {
+                HttpSession session = req.getSession();
+                session.setAttribute("user", username);
+                res.sendRedirect("/profile");
+            } else {
+                res.sendRedirect("/login");
+            }
         }
     }
 }
